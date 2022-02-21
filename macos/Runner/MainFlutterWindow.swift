845e3ad88be895ba828d3f7477b410b1769293f1
杨国杰 <yangguojie@paraview.cn>
2022-02-21 11:31:27 +0800

    RF# 播放器添加

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -1,15 +1,29 @@
 import Cocoa
 import FlutterMacOS
+import KSPlayer
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
-    let flutterViewController = FlutterViewController.init()
-    let windowFrame = self.frame
-    self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
+      let flutterViewController = FlutterViewController.init()
+      let windowFrame = self.frame
+      self.contentViewController = flutterViewController
+      self.setFrame(windowFrame, display: true)
 
-    RegisterGeneratedPlugins(registry: flutterViewController)
-
+      
+      KSPlayerManager.firstPlayerType = KSMEPlayer.self
+      KSPlayerManager.secondPlayerType = KSMEPlayer.self
+      KSPlayerManager.logLevel = .debug
+      KSOptions.isAutoPlay = true
+      KSOptions.isSeekedAutoPlay = true
+      KSOptions.hardwareDecodeH264 = true
+      KSOptions.hardwareDecodeH265 = true
+      
+      RegisterGeneratedPlugins(registry: flutterViewController)
+      VideoPlayerPlugin.register(with: flutterViewController.registrar(forPlugin: "VideoPlayerPlugin"))
+      
+      
+      styleMask = [.closable, .miniaturizable, .resizable, .titled]
+      
     super.awakeFromNib()
   }
 }
